ca32ece7f098bb2cb529f2e518e8640f0add338c
Jerome Wei <jeromew@berkeley.edu>
2018-02-02 12:50:43 -0800

    stuff

--- block.js
+++ block.js
@@ -1,1 +1,2 @@
-window.location = chrome.extension.getURL("blocked.html")
+//this runs once on each page
+window.location = chrome.extension.getURL("blocked.html") //Blocks
---
+++ pomodoro.js
@@ -0,0 +1,5 @@
+//This runs in the background all the time
+//This tells everything else what to do
+window.setInterval(function() {
+    console.log("pomodoro is running")
+}, 1000);
